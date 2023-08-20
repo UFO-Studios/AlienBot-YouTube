20a2ceef2dc8144fe3054234afed9052b3048d2e
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2023-08-20 22:07:34 +0530

    also

--- commands/quote.js
+++ commands/quote.js
@@ -2,22 +2,16 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 const generateRandom = require("../utils/generateRandom.js");
 
-async function getQuote() {
-  const fileLine = generateRandom(1, 24);
-  //read quotes.txt
-
-  const filePath = path.resolve("./txt/quotes.txt");
-  const data = await fs
-    .readFile(filePath, { encoding: "utf-8" })
-    .then(function (d) {
-      return d.toString().split("\n")[fileLine];
-    });
-
-  return data;
-}
+//read quotes.txt
+const filePath = path.resolve("./txt/quotes.txt");
+const data = await fs
+  .readFile(filePath, { encoding: "utf-8" })
+  .then(function (d) {
+    return d.toString().split("\n");
+  });
 
 async function quote(msg) {
-  await msg(await getQuote());
+  await msg(data[generateRandom(1, 24)]);
   return true;
 }
 
