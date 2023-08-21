7a134f69ac214d9bb8e6a829007ebf4041088566
NiceygyLive <niceygy@niceygylive.xyz>
2023-08-21 18:06:24 +0000

    fix starnge error

--- commands/quote.js
+++ commands/quote.js
@@ -4,11 +4,14 @@ const generateRandom = require("../utils/generateRandom.js");
 
 //read quotes.txt
 const filePath = path.resolve("./txt/quotes.txt");
-const data = await fs
+async function data() {
+const data2 = await fs
   .readFile(filePath, { encoding: "utf-8" })
   .then(function (d) {
     return d.toString().split("\n");
   });
+  return data2;
+}
 
 async function quote(msg) {
   await msg(data[generateRandom(1, 24)]);
